faa04cd2e5b68b17879213f6c6fec8bf7cea595a
Wehi <iamwehi@proton.me>
2026-04-14 13:46:19 -0400

    ch.1 solution

--- chapter-01.py
+++ chapter-01.py
@@ -7,7 +7,7 @@ from libnum import has_sqrtmod_prime_power, sqrtmod_prime_power
 b = 3 # for bn128, y^2 = x^3 + 3
 seed = "RareSkills"
 
-x = int(sha256(seed.encode('ascii')).hexdigest(), 16) % field_mod 
+x = int(sha256(seed.encode('ascii')).hexdigest(), 16) % field_mod
 
 entropy = 0
 
@@ -17,17 +17,18 @@ vector_basis = []
 
 n = 4
 
-while not has_sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1):
-    # increment x, so hopefully we are on the curve
-    x = (x + 1) % field_mod
-    entropy = entropy + 1
+for _ in range(n):
+    while not has_sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1):
+        # increment x, so hopefully we are on the curve
+        x = (x + 1) % field_mod
+        entropy = entropy + 1
 
-# pick the upper or lower point depending on if entropy is even or odd
-y = list(sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1))[entropy & 1 == 0]
-point = (FQ(x), FQ(y))
-assert is_on_curve(point, b), "sanity check"
-vector_basis.append(point)
+    # pick the upper or lower point depending on if entropy is even or odd
+    y = list(sqrtmod_prime_power((x**3 + b) % field_mod, field_mod, 1))[entropy & 1 == 0]
+    point = (FQ(x), FQ(y))
+    assert is_on_curve(point, b), "sanity check"
+    vector_basis.append(point)
 
-# new x value
-x = int(sha256(str(x).encode('ascii')).hexdigest(), 16) % field_mod 
+    # new x value
+    x = int(sha256(str(x).encode('ascii')).hexdigest(), 16) % field_mod
 print(vector_basis)
